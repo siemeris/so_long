678792af15dcf504ddd1d009e4b6be734cc4c609
Isa Sierra <siemeris@gmail.com>
2023-11-19 19:11:03 +0100

    Update main.c file with mlx functions

--- src/main.c
+++ src/main.c
@@ -6,24 +6,44 @@
 /*   By: issierra <issierra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 10:12:18 by issierra          #+#    #+#             */
-/*   Updated: 2023/11/18 17:09:32 by issierra         ###   ########.fr       */
+/*   Updated: 2023/11/19 19:08:09 by issierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <mlx.h>
 #include "../mlx/mlx.h"
 
-int main(void)
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
-	mlx_ptr = mlx_init();
-	if (!mlx_ptr)
-		return (1);
-	win_ptr = mlx_new_window (mlx_ptr, 300, 300, "Hello World");
-	if (!win_ptr)
-		return (2);
-	mlx_loop(mlx_ptr);
-	return (0);
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init(); //Inicilaizamos la miniLibX y guardamos el puntero en mlx 
+	mlx_win = mlx_new_window(mlx, 300, 300, "Hello world!"); //Creamos una ventana y guardamos el puntero en mlx_win
+	img.img = mlx_new_image(mlx, 300, 300); //Creamos una imagen y guardamos el puntero en img.img
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian); //mlx_get_data_addr devuelve la direccion de memoria de la imagen
+	my_mlx_pixel_put(&img, 5, 5, 0x00F6FC57); 
+	my_mlx_pixel_put(&img, 5, 10, 0x00F6FC57); 
+	my_mlx_pixel_put(&img, 10, 10, 0x00F6FC57); 
+	my_mlx_pixel_put(&img, 15, 15, 0x00F6FC57); 
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx); //Renderizamos la ventana
 }
