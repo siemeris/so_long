c61c0b7e235f2c9a9f523771b2a116e16b806093
Isa Sierra <siemeris@gmail.com>
2023-11-26 11:53:02 +0100

    norminette

--- mac/src/check_map.c
+++ mac/src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: issierra <issierra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 09:17:04 by issierra          #+#    #+#             */
-/*   Updated: 2023/11/26 10:50:41 by issierra         ###   ########.fr       */
+/*   Updated: 2023/11/26 11:45:28 by issierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,118 +20,103 @@
 // 5. Que el mapa tenga al menos un muro alrededor (1)
 // 6. Que el mapa tenga al menos un camino válido (0)
 
-// int ft_print_error(int error)
-// {
-// 	if (error == 1)
-// 		ft_printf("Error\nMAPA INVALIDO, NO RECTANGULAR\n");
-// 	else if (error == 2)
-// 		ft_printf("Error\nMAPA INVALIDO, CARACTER INVALIDO\n");
-// 	else if (error == 3)
-// 		ft_printf("Error\nMAPA INVALIDO, NO HAY COLECCIONABLES, SALIDA O JUGADOR\n");
-// 	else if (error == 4)
-// 		ft_printf("Error\nMAPA INVALIDO, MAS DE UNA SALIDA\n");
-// 	else if (error == 5)
-// 		ft_printf("Error\nMAPA INVALIDO, MAS DE UN JUGADOR\n");
-// 	else if (error == 6)
-// 		ft_printf("Error\nMAPA INVALIDO, NO RODEADO DE MUROS\n");
-// 	else if (error == 7)
-// 		ft_printf("Error\nMAPA INVALIDO, CAMINO NO VALIDO\n");
-// 	return (0);
-// }
-
-
 int	flood_fill(t_check_map *map, size_t x, size_t y)
 {
-	ft_printf("HELLO FROM FLOOD FILL map_copy0 = %s x = %d, y = %d w = %d h = %d \n", map->map_copy[0], x, y, map->w, map->h);
-	if (x < 0 || x >= map->w || y < 0 || y >= map->h || map->map_copy[y][x] == '1' || map->map_copy[y][x] == 'F')
-        return (0);
-    map->map_copy[y][x] = 'F';
-    flood_fill(map, x + 1, y);
-    flood_fill(map, x - 1, y);
-    flood_fill(map, x, y + 1);
-    flood_fill(map, x, y - 1);
-    return (1);
-}
-
-int check_path(t_check_map *check, t_data *data)
-{
-		//COMPROBAR CAMINO VALIDO
-    ///hacemos una copia del mapa    
-    check->map_copy = ft_calloc(data->map_height + 1, sizeof(char *));
-    if (!check->map_copy)
-        return (0);
-    size_t i = 0;
-    while (i < data->map_height)
-    {
-        check->map_copy[i] = ft_strdup(data->map_read[i]);
-        if (!check->map_copy[i])
-            return (0);
-        i++;
-    }
-    check->w = data->map_width;
-    check->h = data->map_height;
-	flood_fill(check, data->player_x, data->player_y);
-        //Imprimimos map_copy
-    i=0;
-    while (i < check->h)
-    {
-        ft_printf("%s\n", check->map_copy[i]);
-        i++;
-    }
-    //Comprobamos que no haya ningun C y ningun E
-    i=0;
-    while (i < check->h)
-    {
-        if (ft_strchr(check->map_copy[i], 'C') || ft_strchr(check->map_copy[i], 'E'))
-			return(ft_print_error(7));
-        i++;
-    }
+	if (x < 0 || x >= map->w || y < 0 || y >= map->h
+		|| map->map_copy[y][x] == '1' || map->map_copy[y][x] == 'F')
+		return (0);
+	map->map_copy[y][x] = 'F';
+	flood_fill(map, x + 1, y);
+	flood_fill(map, x - 1, y);
+	flood_fill(map, x, y + 1);
+	flood_fill(map, x, y - 1);
 	return (1);
 }
 
-int check_chars(t_check_map *check, t_data *data)
+int	check_path(t_check_map *check, t_data *data)
+{
+	size_t	i;
+
+	i = 0;
+	check->map_copy = ft_calloc(data->map_height + 1, sizeof(char *));
+	if (!check->map_copy)
+		return (0);
+	while (i < data->map_height)
+	{
+		check->map_copy[i] = ft_strdup(data->map_read[i]);
+		if (!check->map_copy[i])
+			return (0);
+		i++;
+	}
+	check->w = data->map_width;
+	check->h = data->map_height;
+	flood_fill(check, data->player_x, data->player_y);
+	i = 0;
+	while (i < check->h)
+	{
+		if (ft_strchr(check->map_copy[i], 'C')
+			|| ft_strchr(check->map_copy[i], 'E'))
+			return (ft_print_error(7));
+		i++;
+	}
+	return (1);
+}
+
+int	check_chars_aux(t_check_map *check, t_data *data)
+{
+	if (data->map_read[check->h][check->w] != '0'
+		&& data->map_read[check->h][check->w] != '1'
+		&& data->map_read[check->h][check->w] != 'C'
+		&& data->map_read[check->h][check->w] != 'E'
+		&& data->map_read[check->h][check->w] != 'P')
+		return (ft_print_error(2));
+	else if (data->map_read[check->h][check->w] == 'C')
+		check->collect++;
+	else if (data->map_read[check->h][check->w] == 'E')
+	{
+		check->exit++;
+		if (check->exit != 1)
+			return (ft_print_error(4));
+	}
+	else if (check->w == 0 || check->h == 0
+		|| check->w == data->map_width - 1
+		|| check->h == data->map_height - 1)
+	{
+		if (data->map_read[check->h][check->w] != '1')
+			return (ft_print_error(6));
+	}
+	return (1);
+}
+
+int	check_chars(t_check_map *check, t_data *data)
 {
 	while (check->h < data->map_height)
 	{
 		while (check->w < data->map_width)
 		{
-			if (data->map_read[check->h][check->w] != '0' && data->map_read[check->h][check->w] != '1' && data->map_read[check->h][check->w] != 'C' && data->map_read[check->h][check->w] != 'E' && data->map_read[check->h][check->w] != 'P')
-				return (ft_print_error(2));
-			else if (data->map_read[check->h][check->w] == 'C')
-			 	check->collect++;
-			else if (data->map_read[check->h][check->w] == 'E')
-			{ 
-			 	check->exit++;
-				if (check->exit != 1)
-					return (ft_print_error(4));
-			}
+			if (!check_chars_aux(check, data))
+				return (0);
 			else if (data->map_read[check->h][check->w] == 'P')
 			{
 				check->player++;
-				data->player_x = check->w; //guardamos la posicion del jugador
+				data->player_x = check->w;
 				data->player_y = check->h;
 				if (check->player > 1)
 					return (ft_print_error(5));
-			}
-			if (check->w == 0 || check->h == 0 || check->w == data->map_width - 1 || check->h == data->map_height - 1)
-			{
-				if (data->map_read[check->h][check->w] != '1')
-					return (ft_print_error(6));
 			}
 			check->w++;
 		}
 		check->w = 0;
 		check->h++;
 	}
-	
 	return (1);
 }
 
-int check_map(t_data *data)
+int	check_map(t_data *data)
 {
 	t_check_map	*check;
 
-    check = ft_calloc(1, sizeof(t_check_map));
+	check = ft_calloc(1, sizeof(t_check_map));
 	if (!check)
 		return (0);
 	data->map_height = 0;
--- mac/src/errors.c
+++ mac/src/errors.c
@@ -6,20 +6,21 @@
 /*   By: issierra <issierra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 10:09:22 by issierra          #+#    #+#             */
-/*   Updated: 2023/11/26 10:09:37 by issierra         ###   ########.fr       */
+/*   Updated: 2023/11/26 11:52:16 by issierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int ft_print_error(int error)
+int	ft_print_error(int error)
 {
 	if (error == 1)
 		ft_printf("Error\nMAPA INVALIDO, NO RECTANGULAR\n");
 	else if (error == 2)
 		ft_printf("Error\nMAPA INVALIDO, CARACTER INVALIDO\n");
 	else if (error == 3)
-		ft_printf("Error\nMAPA INVALIDO, NO HAY COLECCIONABLES, SALIDA O JUGADOR\n");
+		ft_printf("Error\nMAPA INVALIDO, \
+NO HAY COLECCIONABLES, SALIDA O JUGADOR\n");
 	else if (error == 4)
 		ft_printf("Error\nMAPA INVALIDO, MAS DE UNA SALIDA\n");
 	else if (error == 5)
