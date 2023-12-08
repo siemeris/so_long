8d0af34ffee3ac5ab36b38e99997ddf7ed25f588
Isa Sierra <siemeris@gmail.com>
2023-12-08 18:12:35 +0100

    Update Makefile and extras_bonus.c

--- mac/src_bonus/extras_bonus.c
+++ mac/src_bonus/extras_bonus.c
@@ -6,7 +6,7 @@
 /*   By: issierra <issierra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 11:12:36 by issierra          #+#    #+#             */
-/*   Updated: 2023/12/06 09:58:44 by issierra         ###   ########.fr       */
+/*   Updated: 2023/12/08 18:09:36 by issierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	movements_on_screen(t_data *data)
 	char	*ptr;
 
 	ptr = ft_itoa(data->moves);
-	mlx_string_put(data->mlx_ptr, data->win_ptr, 60, 60, 0x00000000, "Moves:");
-	mlx_string_put(data->mlx_ptr, data->win_ptr, 75, 75, 0x00000000, ptr);
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 25, 25, 0x00000000, "Moves:");
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 45, 45, 0x00000000, ptr);
 	free(ptr);
 }
 
